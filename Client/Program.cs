62b4b37042d628a034e5b973502ec9d8f7da0556
Rajan Kumar <rajan_kumar@hotmail.com>
2023-04-01 23:14:42 -0700

    Fix "#App"

--- Client/Program.cs
+++ Client/Program.cs
@@ -22,7 +22,7 @@ namespace Client
 
 
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
