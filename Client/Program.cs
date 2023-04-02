b4b28db26bc60027ace2231a3b3595fe3896224f
Rajan Kumar <rajan_kumar@hotmail.com>
2023-04-01 22:55:03 -0700

    Added authentication

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Azure.Functions.Authentication.WebAssembly;
-using Microsoft.Extensions.DependencyInjection;
+
 
 namespace Client
 {
@@ -11,12 +11,28 @@ namespace Client
     {
         public static async Task Main(string[] args)
         {
+            //var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            //builder.RootComponents.Add<App>("app");
+
+            //builder.Services.AddScoped(sp => new HttpClient 
+            //                    { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) })
+            //                .AddStaticWebAppsAuthentication();
+
+            //await builder.Build().RunAsync();
+
+
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient 
-                                { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) })
-                            .AddStaticWebAppsAuthentication();
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddOidcAuthentication(options =>
+            {
+                // Configure your authentication provider options here.
+                // For more information, see https://aka.ms/blazor-standalone-auth
+                builder.Configuration.Bind("Cognito", options.ProviderOptions);
+            });
 
             await builder.Build().RunAsync();
         }
